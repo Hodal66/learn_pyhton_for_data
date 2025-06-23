a9f4484d3b66480ec01e7f629f3751d8a8668181
MUHETO Hodal <mhthodol@gmail.com>
2025-06-23 19:04:21 +0200

    This id How functions works in python

--- index.py
+++ index.py
@@ -5,3 +5,27 @@ sum=number1+number2
 print(sum)
 
 print("hello again")
+#this is the comment in Python
+
+fullName='Muheto Hodal'
+email ="mhthodol@gmail.com"
+phone_number= 782439775
+is_boy = True
+price = 9.67
+
+
+print('Your name is ', fullName, ' Your Email is ', email, ' your Phone number is ', phone_number)
+
+
+#Function Declaration
+
+def greet_and_check_age(name,age):
+    if name == "Eric" :
+        print("Hello, Eric")
+        
+    if age >= 18 :
+        print("you are an adult.")
+    else:
+        print("you are a Minor")
+        
+greet_and_check_age("Hodal", 36)
